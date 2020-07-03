6dc0c57539b150e3d5c4d771715ed3e3dabe03de
raghueswar55 <raghueswar55@gmail.com>
2020-07-03 08:00:19 +0530

    Initiated rest api project.

---
+++ GreetingAPI/src/test/java/com/bridgelabz/greetingapi/GreetingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
