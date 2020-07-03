7d61f27b3f5e7c3cdede59e24418af7be518d0ec
raghueswar55 <raghueswar55@gmail.com>
2020-07-03 11:12:49 +0530

    Implimented test csaes.

--- GreetingAPI/src/test/java/com/bridgelabz/greetingapi/GreetingApiApplicationTests.java
+++ GreetingAPI/src/test/java/com/bridgelabz/greetingapi/GreetingApiApplicationTests.java
@@ -1,13 +1,47 @@
 package com.bridgelabz.greetingapi;
 
+import com.sun.tools.javac.util.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 @SpringBootTest
 class GreetingApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void givenCurlCommandWithUrlEndPointHello_GreetingAPI_shouldReturnHelloWorld() {
+        try {
+            Process process = Runtime.getRuntime().exec("curl http://localhost:8080/hello/home");
+            String result = "";
+            Scanner scanner = new Scanner(process.getInputStream());
+            scanner.useDelimiter("\r\n");
+            while (scanner.hasNext())
+                result = result+scanner.next();
+            scanner.close();
+            Assert.check(result.equals("Hello World!..."));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Test
+    void givenCurlCommandWithUrlEndPointAsEmpty_GreetingAPI_shouldReturnHelloWorld() {
+        try {
+            Process process = Runtime.getRuntime().exec("curl http://localhost:8080/hello");
+            String result = "";
+            Scanner scanner = new Scanner(process.getInputStream());
+            scanner.useDelimiter("\r\n");
+            while (scanner.hasNext())
+                result = result+scanner.next();
+            scanner.close();
+            System.out.println(result);
+            Assert.check(result.equals("Hello World!..."));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
