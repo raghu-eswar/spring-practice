476a40adfdc7593e4c93cbd4ed32823bfebfc84e
raghueswar55 <raghueswar55@gmail.com>
2020-07-03 13:35:09 +0530

    Modified functionality names.

--- GreetingAPI/src/main/java/com/bridgelabz/greetingapi/controller/GreetingController.java
+++ GreetingAPI/src/main/java/com/bridgelabz/greetingapi/controller/GreetingController.java
@@ -12,13 +12,13 @@ public class GreetingController {
     }
 
     @GetMapping("user")
-    public String sayHelloUsingParameters(@RequestParam(value = "firstName") String firstName,
+    public String greetUserInParameters(@RequestParam(value = "firstName") String firstName,
                                           @RequestParam(value = "lastName") String lastName) {
         return "Hello "+firstName+" "+lastName+" ...";
     }
 
     @GetMapping("user/{firstName}/{lastName}")
-    public String sayHelloUsingPath(@PathVariable String firstName, @PathVariable String lastName) {
+    public String greetUserInPath(@PathVariable String firstName, @PathVariable String lastName) {
         return "Hello "+firstName+" "+lastName+" ...";
     }
 
